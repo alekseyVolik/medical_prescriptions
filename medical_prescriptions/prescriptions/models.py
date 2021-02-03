d52b121b4f9233f1a1bbe29c93c621e63b9ca087
smalltownboy <volikjob@gmail.com>
2021-02-03 16:44:06 +0400

    Update prescriptions.models
    Added following models:
    
    Patient
    - first_name
    - last_name
    - father_name
    - phone_number
    
    Doctor
    - first_name
    - last_name
    - father_name
    - phone_number
    
    Prescription
    - description
    - patient
    - doctor
    - create_date
    - validity
    - priority

--- medical_prescriptions/prescriptions/models.py
+++ medical_prescriptions/prescriptions/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Patient(models.Model):
+    first_name = models.CharField(max_length=50, verbose_name='Имя')
+    last_name = models.CharField(max_length=50, verbose_name='Фамилия')
+    father_name = models.CharField(max_length=50, verbose_name='Отчество')
+    phone_number = models.CharField(max_length=50, verbose_name='Телефон')
+
+    class Meta:
+        verbose_name = 'Пациент'
+        verbose_name_plural = 'Пациенты'
+        ordering = ['first_name']
+
+
+class Doctor(models.Model):
+    first_name = models.CharField(max_length=50, verbose_name='Имя')
+    last_name = models.CharField(max_length=50, verbose_name='Фамилия')
+    father_name = models.CharField(max_length=50, verbose_name='Отчество')
+    specialization = models.CharField(max_length=75, verbose_name='Специализация')
+
+    class Meta:
+        verbose_name = 'Доктор'
+        verbose_name_plural = 'Доктора'
+        ordering = ['specialization', 'first_name']
+
+
+class Prescription(models.Model):
+
+    PRIORITY_TYPES = (('Нормальный', 'Нормальный'),
+                      ('Срочный', 'Срочный'),
+                      ('Незамедлительный', 'Незамедлительный'))
+
+    description = models.TextField()
+    patient = models.ForeignKey('Patient', null=True, on_delete=models.PROTECT, verbose_name='Пациент')
+    doctor = models.ForeignKey('Doctor', null=True, on_delete=models.PROTECT, verbose_name='Доктор')
+    create_date = models.DateField(auto_now_add=True, verbose_name='Создан')
+    validity = models.DurationField(verbose_name='Срок действия')
+    priority = models.CharField(max_length=20, verbose_name='Приоритет', choices=PRIORITY_TYPES)
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+        ordering = ['create_date']
