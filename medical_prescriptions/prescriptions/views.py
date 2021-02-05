b88dca4b328d189de5ab33673c458dbcdaa05d8a
smalltownboy <volikjob@gmail.com>
2021-02-05 14:38:43 +0400

    Update views.py
    add success url to index page for following view controller-class:
    - CreatePatient
    - CreateDoctor
    - CreatePrescription

--- medical_prescriptions/prescriptions/views.py
+++ medical_prescriptions/prescriptions/views.py
@@ -8,21 +8,21 @@ class CreatePatient(CreateView):
     model = Patient
     fields = ('first_name', 'last_name', 'father_name', 'phone_number')
     template_name_suffix = '_create'
-    success_url = ''
+    success_url = reverse_lazy('index')
 
 
 class CreateDoctor(CreateView):
     model = Doctor
     fields = ('first_name', 'last_name', 'father_name', 'specialization')
     template_name_suffix = '_create'
-    success_url = ''
+    success_url = reverse_lazy('index')
 
 
 class CreatePrescription(CreateView):
     model = Prescription
     fields = ('description', 'patient', 'doctor', 'validity', 'priority')
     template_name_suffix = '_create'
-    success_url = ''
+    success_url = reverse_lazy('index')
 
 
 class PrescriptionIndex(TemplateView):
