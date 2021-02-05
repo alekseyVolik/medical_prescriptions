c696c6b8e116fab9848c9dbc7682ee592e7a7411
smalltownboy <volikjob@gmail.com>
2021-02-05 13:40:07 +0400

    Refactor code
    Fixed some errors

--- medical_prescriptions/prescriptions/urls.py
+++ medical_prescriptions/prescriptions/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from .views import CreatePatient, CreateDoctor, CreatePrescription
+from .views import PrescriptionIndex
 
 
 urlpatterns = [
-    path('', '', name='index'),
+    path('', PrescriptionIndex.as_view(), name='index'),
     path('create/patient', '', name='create_patient'),
     path('create/doctor', '', name='create_doctor'),
-    path('create/prescription', '', name='create_prescription')
+    path('create/prescription', '', name='create_prescription'),
 ]
