2ce6f120f4935ed4095a56702475224a2604e356
smalltownboy <volikjob@gmail.com>
2021-02-05 11:03:15 +0400

    Update views.py
    Added 3 view controller-class with empty success url

--- medical_prescriptions/prescriptions/views.py
+++ medical_prescriptions/prescriptions/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from .models import Patient, Doctor, Prescription
+from django.views.generic import CreateView
 
-# Create your views here.
+
+class CreatePatient(CreateView):
+    model = Patient
+    fields = ('first_name', 'last_name', 'father_name', 'phone_number')
+    template_name_suffix = '_create'
+    success_url = ''
+
+
+class CreateDoctor(CreateView):
+    model = Doctor
+    fields = ('first_name', 'last_name', 'father_name', 'specialization')
+    template_name_suffix = '_create'
+    success_url = ''
+
+
+class CreatePrescription(CreateView):
+    model = Prescription
+    fields = ('description', 'patient', 'doctor', 'validity', 'priority')
+    template_name = '_create'
+    success_url = ''
