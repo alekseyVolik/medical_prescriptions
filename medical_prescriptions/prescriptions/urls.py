ef24b794848bde3e87df3fb2af4b5850ee0ef662
smalltownboy <volikjob@gmail.com>
2021-02-09 10:53:08 +0400

    update urls from prescriptions
    added three urls for each view that show Patients, Doctors, Prescriptions

--- medical_prescriptions/prescriptions/urls.py
+++ medical_prescriptions/prescriptions/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from .views import CreatePatient, CreateDoctor, CreatePrescription
+from .views import Patients, Doctors, Prescriptions
 from .views import PrescriptionIndex
 
 
@@ -9,4 +10,7 @@ urlpatterns = [
     path('create/patient', CreatePatient.as_view(), name='create_patient'),
     path('create/doctor', CreateDoctor.as_view(), name='create_doctor'),
     path('create/prescription', CreatePrescription.as_view(), name='create_prescription'),
+    path('show/patients', Patients.as_view(), name='show_patients'),
+    path('show/doctors', Doctors.as_view(), name='show_doctors'),
+    path('show/prescriptions', Prescriptions.as_view(), name='show_prescriptions'),
 ]
